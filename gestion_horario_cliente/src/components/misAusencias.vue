0b82dc90ecc5de5e24854921893bc310a06aaa5e
Luis <luis.vazquez.franco.al@iespoligonosur.org>
2024-05-31 23:21:33 +0200

    eliminado intento de modal

--- gestion_horario_cliente/src/components/misAusencias.vue
+++ gestion_horario_cliente/src/components/misAusencias.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="mt-1.5 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2">
-      <button @click="showModal = true" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Crear Ausencia</button>
+      <a href="/crearAusencia" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Crear Ausencia</a>
     </div>
 
     <div id="ausencias-container" class="mt-4">
@@ -17,7 +17,6 @@ export default {
     return {
       ausencias: [],
       editMode: {},
-      showModal: false, // Estado para controlar la visibilidad del modal
     };
   },
   async mounted() {
@@ -262,3 +261,4 @@ export default {
 <style scoped>
 /* Agrega tus estilos aquí */
 </style>
+
